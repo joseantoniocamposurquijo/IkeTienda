35ede33cdfce65066710f387433886c27e5ea85b
joseantoniocamposurquijo <jac146@yahoo.es>
2021-08-24 11:03:32 -0500

    modificacion archivos config.php y readme.doc

--- Config/Config.php
+++ Config/Config.php
@@ -1,30 +1,30 @@
 <?php 
 
 // Ubicación de la aplicación
-const APPURL = 'http://localhost/Tienda/';
-const PROTOCOL = 'http';
-const APPPATH = 'C:/Apache24/htdocs/Tienda/';
+const APPURL = 'http://localhost/Tienda/'; // url de la aplicacion
+const PROTOCOL = 'http'; // protocolo a usar
+const APPPATH = 'C:/Apache24/htdocs/Tienda/'; // path donde se encuentra ubicado el proyecto
 
 // Url donde se visualizan las imagenes
-const URLIMG = 'http://localhost/Tienda/Assets/Img/Uploads/';
+const URLIMG = 'http://localhost/Tienda/Assets/Img/Uploads/'; // url de descarga de imagenes
 
 // Constantes de base de datos
-const DBHOST = 'localhost';
-const DBNAME = 'tienda';
-const DBUSER = 'root';
-const DBPW = '1q2w3e4r';
-const DBCHARSET = 'latin1';
+const DBHOST = 'localhost'; // servidor de base de datos
+const DBNAME = 'tienda'; // nombre de la base de datos
+const DBUSER = 'root'; // usuario
+const DBPW = '1q2w3e4r'; // password
+const DBCHARSET = 'latin1'; // set de caracteres
 
 // Separadores para la funcion formatMoney()
-const SPD = ',';
-const SPM = '.';
-const SMONEY = '$';
+const SPD = ','; // separadores de decimales
+const SPM = '.'; // separadore de miles
+const SMONEY = '$'; // simbolo de moneda
 
 // Establecimiento de zona horaria
 date_default_timezone_set("America/Bogota");
 
 // Modo de encriptacion de los valores pasados en el carrito de compras
-const KEY = 'ike';
-const METHOD = 'AES-128-ECB';
+const KEY = 'ike'; // clave para encriptar info
+const METHOD = 'AES-128-ECB'; // metodo de encriptacion
 
 ?>
